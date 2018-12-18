db5a648714eb6d7a5ac36bd0b44ea2be2476ce0e
ErnieBuyco <ernie.buyco@fmclarity.com>
2018-12-18 13:08:21 +0800

    eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 
-
 const Header = "hello there";
-
 class App extends Component {
     state = {
          persons : [
@@ -11,8 +9,10 @@ class App extends Component {
              {name: 'Richard', age: 40},
              {name: 'Eliamer', age: 29}
          ],
-         otherState: 'some other value'
+         otherState: 'some other value',
+         showPersons: false
     }
+
 
     switchNameHandler = () => { 
         //Dont do this  --- this.state.persons[0].name = 'Hahaha';
@@ -25,13 +25,57 @@ class App extends Component {
         })
     }
 
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'newName', age: 50},
+                {name: event.target.value , age: 90},
+                {name: 'Stephanie', age: 12}
+            ]
+        })
+    }
+
+    togglePersonHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    }
+
     render(){
+        const btn  = {
+            backgroundColor: '#00C05E',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer',
+            textAlign: 'center',
+            justifyContent: 'center',
+            margin: '0 auto',
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: '0.5rem',
+            color: '#ffffff',
+            fontWeight: '600'
+           
+        }
+
         return(
             <div className="App">
             <h1 style={{textAlign: "center"}}>{Header}</h1>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
-             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-             <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing</Person>      
+            <button 
+            style={btn}
+            onClick={this.togglePersonHandler}>Toggle Persons</button>
+           { 
+               this.state.showPersons === true ? 
+           <div>
+           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+             <Person 
+             name={this.state.persons[1].name} 
+             age={this.state.persons[1].age}
+             click={this.switchNameHandler.bind(this, 'Maxer!')}
+             changed={this.nameChangeHandler}
+             > My Hobbies: Racing</Person>  
+           </div> : null
+           }    
             </div>
         )
        
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import '../Person/Person.css';
 
-const person = (props) => {  
+
+const person = ( props ) => {  
    return (
-       <div>
+       <div className="Person">
             <p>I'm {props.name} and I am {props.age} years of age!</p>
             <p>{props.children}</p>
+            <input 
+            type="text"
+            onChange={props.changed}
+            value={props.name} />
        </div>
   
    );
 };
+
 
 
 export default person;
